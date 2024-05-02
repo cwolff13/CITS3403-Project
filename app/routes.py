51b6389a5748b5a07e5166b38a953b5b7c7ae258
cwolff13 <129923169+cwolff13@users.noreply.github.com>
2024-05-03 07:20:29 +0800

    Render the Templates (login and catching)
    - Render the templates for each html page
    - Ensure the styling done previously is preserved (URL for instead of paths)
    - establish the folder architecture of /static and /templates necessary for flask
    (in retrospect one commit per template would be the most mutually exclusive, but this occurred to me after completing this work.)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/catching')
+def catching():
+    return render_template('catching.html')
